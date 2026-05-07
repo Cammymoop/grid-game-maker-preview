5f0475af9e708e3b1ba1e0101c447bcad30a2be4
Cammymoop <felixtheslayer@gmail.com>
2026-05-07 11:58:19 -0500

    listing sprite effect ideas

---
+++ src/Effects/sprite_effects.gd
@@ -0,0 +1,63 @@
+class_name SpriteEffects
+extends Node
+
+const StaticEffects: Array[String] = [
+    "Smaller",
+    "Bigger",
+
+    "Shattered",
+    
+    "Colored",
+    
+    "Angled",
+]
+
+const AnimatedEffects: Array[String] = [
+    "Grow In",
+    "Grow Out",
+    "Shrink In",
+    "Shrink Out",
+    
+    "Fall In",
+    "Fall Out",
+    
+    "Slide In",
+    "Slide Out",
+
+    "Spin In",
+    "Spin Out",
+    
+    "Fade In",
+    "Fade Out",
+    
+    "Color In",
+    "Color Out",
+    
+    "Scan In",
+    "Scan Out",
+    "Scanning",
+    
+    "Dissolve In",
+    "Dissolve Out",
+    
+    "Pixel Explode",
+    "Shatter Explode",
+    
+    "Spinning",
+    "Shaking",
+    
+    "Size Bounce",
+    "Angle Bounce",
+    "Fade Bounce",
+    "Hop Bounce",
+    "Color Bounce",
+]
+
+const ParticleEffects: Array[String] = [
+    "Sparkle",
+    "Burning Smoke",
+    "Fog",
+    "Smoke Bomb",
+    "Electric",
+    "Bubbles",
+]
